c57a0e22456dce79937b344fb4c59b8f5e74a782
Sedclerk <seclerk@gmail.com>
2013-08-16 15:20:08 +0300

    Changes

--- project13.rb
+++ project13.rb
@@ -2,42 +2,54 @@ require "mysql"
 
 class Account
     
-    attr_accessor :account_holder
-    
+    attr_accessor :account_holder, :balance
+
     def initialize(account_holder)
         @account_holder = account_holder
-        @db = Mysql.connect'localhost', 'root', 'Sedclerk13','bankbe'
-        st = @db.query("INSERT INTO accounts(name, balance) VALUES ( account_holder , 0)")
+        @balance = 0
+        
+        @db = Mysql.new 'localhost', 'root', 'Sedclerk13', 'tsiky'
+        st = @db.query("INSERT INTO accounts(name, balance) VALUES ( '#{account_holder}' , 0)")
     end
-    
+
     def balance
-        st = @db.query("SELECT balance FROM accounts WHERE name = ?")
-        results.each_hash do |row|
-            puts "The holder name is #{row.name}: the balance is #{row.balance}"
-        end
+        bal = balance()
+        st = @db.query("SELECT balance FROM accounts WHERE name = ('#{account_holder}')")
+        st.execute(bal)
+        st.close if st
     end
-    
+
     def deposit(money)
-        vola = money[:balance]
-        money[:balance]= money[:balance] + money
-        st = @db.prepare('INSERT into accounts (balance) VALUES (?)')
-        st.execute(vola)
+        bal = balance()
+        new_balance = money + bal
+        st = @db.prepare("INSERT INTO accounts balance VALUES ('#{new_balance}')")
+        st.execute (new_balance)
+        st.close if st
     end
-    
+
     def withdraw(money)
-        vola = money[:balance]
-        money[:balance]= money[:balance] - money
-        st = @db.prepare('INSERT into accounts (balance) VALUES (?)')
-        st.execute(vola)
+        bal = balance()
+        new_balance = money - bal
+        st = @db.prepare("INSERT INTO accounts (balance) VALUES ('#{new_balance}')")
+        st.execute (new_balance)
+        st.close if st
+    end
+
+    def transfer(money, target_account)
+        bal = balance()
+        new_balance = money - bal
+        target_account.deposit(money)
+        st = @db.prepare("INSERT INTO accounts (balance) VALUES ('#{new_balance}')")
+        st.execute (new_balance)
+        st.close if st
     end
     
-    def transfer(amount, target_account)
-        vola = money[:balance]
-        money[:balance]= money[:balance] - money
-        target_account.deposit(amount)
-        st = @db.prepare('INSERT into accounts (balance) VALUES (?)')
-        st.execute(vola)
+    def display
+        
+        puts "Your Name's account: "+ @account_holder
+        puts "Your Balance: " + @balance.to_s
     end
+    
 end
 
 a = Account.new('Steve')
