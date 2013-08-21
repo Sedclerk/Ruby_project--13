57c1b51abf906436ff12adf1370de4c5f5e8b46a
Sedclerk <seclerk@gmail.com>
2013-08-21 22:44:07 +0300

    last change today

--- latest13.rb
+++ latest13.rb
@@ -7,18 +7,18 @@ class Account
     def initialize(account_holder)
         @account_holder = account_holder
         
-        @db = Mysql.new 'localhost', 'root', 'Sedclerk13', 'tsiky'
+        @db = Mysql.new 'localhost', 'root', 'Sedclerk13', 'Tsiky'
         st = @db.query("INSERT INTO accounts(name, balance) VALUES ( '#{account_holder}' , 0)")
     end
     
     def balance
-        bal = 0
         st = @db.query("SELECT balance FROM accounts WHERE name = '#{account_holder}'")
+        bal = st.at(0)
         return bal
     end
     
     def find(name)
-        st = db.query("SELECT * FROM accounts WHERE name = '#{account_holder}'")
+        st = @db.query("SELECT * FROM accounts WHERE name = '#{account_holder}'")
         st.execute(name)
         st.each {|x| print x, " "}
     end
@@ -26,39 +26,39 @@ class Account
     def deposit(money)
         bal = balance()
         new_balance = bal + money
-        st = @db.query("UPDATE accounts SET balance = '#{new_balance}'  WHERE name = '#{account_holder}'")
+        bal = @db.query("UPDATE accounts SET balance = '#{new_balance}'  WHERE name = '#{account_holder}'")
     end
     
     def withdraw(money)
         bal = balance()
         new_balance = bal - money
-        st = @db.query("UPDATE accounts SET balance = '#{new_balance}'  WHERE name = '#{account_holder}'")
+        bal = @db.query("UPDATE accounts SET balance = '#{new_balance}'  WHERE name = '#{account_holder}'")
         
     end
     
     def transfer(money, target_account)
         bal = balance()
         new_balance = bal - money
-        st = @db.query("UPDATE accounts SET balance = '#{new_balance}'  WHERE name = '#{account_holder}'")
+        bal = @db.query("UPDATE accounts SET balance = '#{new_balance}'  WHERE name = '#{account_holder}'")
         target_account.deposit(money)
     end
 end
 
-a = Account.new('BOB')
+a = Account.new('Steven')
 a.deposit(100)
 puts a.balance
 
-b = Account.new('Ketaka')
-puts b.display
+b = Account.new('Jerons')
+
 b.deposit(50)
 puts b.balance
 
-c = Account.find('Steve')
-puts c.balance
+
+puts a.find('Steven')
+
+
+puts b.find('Jerons')
 
 a.transfer(25, b)
 puts a.balance
-
-d = Account.find('Jeron')
-puts d.balance
 
